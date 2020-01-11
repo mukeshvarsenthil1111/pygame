e6c759aa37cb10c47364b546724e3e0fbaaf8550
mukeshvarsenthil1111 <smukeshvar@gmail.com>
2020-01-11 06:04:33 +0000

    Latest Commit

--- main.py
+++ main.py
@@ -11,13 +11,13 @@ pygame.init()
 display_width = 800
 display_height = 600
  
-#black = (0,0,0)
-#white = (255,255,255)
+black = (0,0,0)
+white = (255,255,255)
 
-#red = (200,0,0)
+red = (200,0,0)
 green = (0,200,0)
 
-#bright_red = (255,0,0)
+bright_red = (255,0,0)
 bright_green = (0,255,0)
  
 block_color = (53,115,255)
